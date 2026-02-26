ca722f71b68053b9a63bc3d299e1aea356506db7
JONATA HENRIQUE DA SILVA CARÁ <26382@unifacef.edu.br>
2026-02-26 20:34:13 -0300

    26/02

--- ClasseObjeto/src/Carro.java
+++ ClasseObjeto/src/Carro.java
@@ -1,15 +1,53 @@
 public class Carro{
-    // variáveis ou propriedades ou atributos
+    // variáveis ou propriedade ou atributos
     public String marca, modelo; // String é um tipo de dado do tipo classe
     public float velocidade; // float é um tipo de dado primitivo
     public boolean motor; // boolean é um tipo de dado primitivo
 
-    // metodo que converte o objeto em String
+    // método construtor - tem o mesmo nome da classe
+    public Carro(String marca, String modelo, float velocidade, boolean motor){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidade = velocidade;
+        this.motor = motor;
+    }
+
+    // método para ligar o carro
+    public void ligarCarro(){
+        if (!this.motor) {
+            this.motor = true;
+            System.out.println("Motor ligado.");
+        }
+    }
+    public void desligarCarro(){
+        if (this.motor){
+            this.motor = false;
+            this.velocidade = 0;
+            System.out.println("Motor desligado.");
+        }
+    }
+    public void aumentarVelocidade(float v){
+        if (this.motor) {
+            this.velocidade += v;
+            System.out.println("O carro esta a " + this.velocidade + " km/h.");
+        }
+    }
+
+    public void reduzirVelocidade(float r){
+        if (this.motor && this.velocidade - r >= 0){
+            this.velocidade -= r;
+            System.out.println("Velocidade reduzida para " + this.velocidade + " km/h.");
+        }
+    }
+
+    // método que converte o objeto em String
     public String toString(){
-        // this representa o atual objeto acionando o metodo
-        return "Marca: " + this.marca +
+        // this representa o objeto que chama o método
+        return " Marca: " + this.marca +
                 " Modelo: " + this.modelo +
                 " Velocidade: " + this.velocidade +
                 " Motor: " + this.motor;
     }
+
+
 }
--- ClasseObjeto/src/TestaCarro.java
+++ ClasseObjeto/src/TestaCarro.java
@@ -1,18 +1,19 @@
 public class TestaCarro {
     public static void main(String[] args){
-        // instanciar novo objeto
-        Carro obj1 = new Carro();
-        obj1.marca = "Fiat";
-        obj1.modelo = "Uno";
-        obj1.velocidade = 0;
-        obj1.motor = false;
-        System.out.println(obj1);
+        // instanciar objeto obj1
+        Carro obj1 = new Carro("Fiat", "Uno", 0, false);
+        System.out.println(obj1.toString());
 
-        Carro obj2 = new Carro();
-        obj2.marca = "GM";
-        obj2.modelo = "Onix";
-        obj2.velocidade = 0;
-        obj2.motor = false;
-        System.out.println(obj2);
+        // instanciar objeto obj2
+        Carro obj2 = new Carro("GM", "Onix", 0, false);
+        System.out.println(obj2.toString());
+        obj2.ligarCarro();
+        System.out.println(obj2.toString());
+        obj2.desligarCarro();
+        System.out.println(obj2.toString());
+        obj2.ligarCarro();
+
+        obj2.aumentarVelocidade(80);
+        obj2.reduzirVelocidade(30);
     }
 }
