2834122ab0ccd47de074e7849bc561fe704282f0
Jonata Henrique <jonatah.1512@gmail.com>
2026-02-27 13:37:05 -0300

    Exe1 - Conta

---
+++ PP1/Exe1-Conta/Conta/src/Conta.java
@@ -0,0 +1,56 @@
+public class Conta{
+    public int numeroConta, agencia;
+    public String nomeCliente;
+    public float saldo;
+    public boolean status;
+
+    public Conta(int numeroConta, int agencia, String nomeCliente){
+        this.numeroConta = numeroConta;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = 0;
+        this.status = true;
+    }
+
+    public void depositar(float valor){
+        if (this.status){
+            this.saldo += valor;
+            System.out.println("- Depósito realizado.");
+        } else{
+            System.out.println("* Conta inativa *");
+        }
+    }
+
+    public void sacar(float valor){
+        if (this.status){
+            if (this.saldo >= valor){
+                this.saldo -= valor;
+                System.out.println("- Saque realizado. Novo saldo: $" + this.saldo);
+            } else {
+                System.out.println("* Saldo indisponível *");
+            }
+        } else{
+            System.out.println("* Conta inativa *");
+        }
+    }
+
+    public void encerrar(){
+        if (this.status && this.saldo == 0){
+            this.status = false;
+            System.out.println("- Conta encerrada.");
+        } else{
+            System.out.println("* Para encerrar a conta retire o saldo restante *");
+        }
+    }
+
+    public String toString(){
+        return "--------------------------" +
+                "\nNúmero: " + this.numeroConta +
+                "\nAgência: " + this.agencia +
+                "\nCliente: " + this.nomeCliente +
+                "\nSaldo atual: " + this.saldo +
+                "\nStatus da conta: " + this.status +
+                "\n--------------------------";
+    }
+
+}
---
+++ PP1/Exe1-Conta/Conta/src/TestaConta.java
@@ -0,0 +1,18 @@
+public class TestaConta {
+    public static void main(String[] args){
+        Conta c1 = new Conta(123, 10, "João Silva");
+
+        c1.depositar(500);
+        c1.sacar(200);
+        System.out.println(c1.toString());
+
+        Conta c2 = new Conta(321, 11, "Maria Souza");
+        System.out.println(c2.toString());
+        c2.sacar(50);
+
+        c1.encerrar();
+        c1.sacar(c1.saldo);
+        c1.encerrar();
+        System.out.println(c1.toString());
+    }
+}
