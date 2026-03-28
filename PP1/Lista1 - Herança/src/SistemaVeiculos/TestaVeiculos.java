569974f7b0382044d0a30078600367db23260ec8
Jonata Henrique <jonatah.1512@gmail.com>
2026-03-28 10:56:42 -0300

    Lista1 - Herança

---
+++ PP1/Lista1 - Herança/src/SistemaVeiculos/TestaVeiculos.java
@@ -0,0 +1,15 @@
+package SistemaVeiculos;
+
+public class TestaVeiculos {
+    public static void main(String[] args){
+        Aviao av1 = new Aviao("Avião1", "Modelo1", 0, 2000);
+        CarroEletrico cr1 = new CarroEletrico("BYD", "Dolphin", 0, 100);
+
+        Veiculo veiculo = av1;
+        veiculo.mover();
+
+        veiculo = cr1;
+        veiculo.mover();
+
+    }
+}
