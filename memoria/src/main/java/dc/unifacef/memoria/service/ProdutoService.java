ed68654fa1be5c45a62eb9ef62ba358876b1bbcf
26382 <jonatah.1512@gmail.com>
2026-05-12 22:16:39 -0300

    12/05

--- memoria/src/main/java/dc/unifacef/memoria/service/ProdutoService.java
+++ memoria/src/main/java/dc/unifacef/memoria/service/ProdutoService.java
@@ -1,6 +1,8 @@
 package dc.unifacef.memoria.service;
 
 import dc.unifacef.memoria.model.Produto;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -24,5 +26,23 @@ public class ProdutoService {
         id++;
         this.produtos.add(produto);
         return produto;
+    }
+
+    // Remove um produto com id
+    public boolean remove(Long id){
+        return this.produtos.removeIf(produto -> produto.getId().equals(id));
+    }
+
+    // Atualiza um produto por id
+    public Produto atualiza(Long id, Produto produto){
+        produto.setId(id);
+        // Percorre para atualizar o produto
+        for(int i = 0; i < produtos.size(); i++){
+            if(produtos.get(i).getId().equals(id)){
+                produtos.set(i, produto);
+                return produto;
+            }
+        }
+        return null;
     }
 }
